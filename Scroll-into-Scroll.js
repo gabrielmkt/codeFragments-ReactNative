ed9de3c4304f089298c5679784b74671819a4fb4
Gabriel Braga <44377180+gabrielmkt@users.noreply.github.com>
2020-11-30 21:14:08 -0300

    create frag

---
+++ Scroll-into-Scroll.js
@@ -0,0 +1,68 @@
+...
+<ScrollView
+                nestedScrollEnabled={true}
+                horizontal={true}
+                pagingEnabled={true}
+                showsHorizontalScrollIndicator={false}
+            >
+                <View style={{ flex: 1, width: width }}>
+                    <ScrollView
+                        nestedScrollEnabled={true}
+                        showsVerticalScrollIndicator={false}
+                    >
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                    </ScrollView>
+                </View>
+
+                <View style={{ flex: 1, width: width }}>
+                    <ScrollView
+                        nestedScrollEnabled={true}
+                        showsVerticalScrollIndicator={false}
+                    >
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                    </ScrollView>
+                </View>
+
+                <View style={{ flex: 1, width: width }}>
+                    <ScrollView
+                        nestedScrollEnabled={true}
+                        showsVerticalScrollIndicator={false}
+                    >
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                        <Text style={{ fontSize: 40 }}> Teste aqui</Text>
+                    </ScrollView>
+                </View>
+            </ScrollView>
+ ...
